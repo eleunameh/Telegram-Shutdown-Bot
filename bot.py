97ca340fee01877780eb16fd59e420e050a28e9e
Eleuname <iamemanuele@gmail.com>
2018-03-24 09:18:01 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import os
 
 
 def main():
-    bot = telegram.Bot(token='bot token')  #Token is needed to make the bot work
+    bot = telegram.Bot(token='bot token')  #Bot Token is needed to make the bot work
 
     global LAST_UPDATE_ID
     LAST_UPDATE_ID = bot.getUpdates()[-1].update_id  
@@ -20,13 +20,13 @@ def main():
                 	try:
                        os.system('shutdown /s /t 5')
                         LAST_UPDATE_ID = update_id
-                        bot.sendMessage(chat_id=update.message.chat_id, text="I shut down your computer, senpai! <3")
+                        bot.sendMessage(chat_id=update.message.chat_id, text="I shut down your computer, senpai! <3") #Sends a message that means the shut down has successfully happened.
                    	except:
                         	pass
                 if text=="reboot" and username=="username":  #Checks if the message is sent by the right user and the content 
                         os.system('shutdown /r /t 5')
                         LAST_UPDATE_ID = update_id
-                        bot.sendMessage(chat_id=update.message.chat_id, text="I rebooted your computer, senpai! <3")
+                        bot.sendMessage(chat_id=update.message.chat_id, text="I rebooted your computer, senpai! <3") #Sends a message that means the reboot has successfully happened.
                 if text=="Who is your daddy?" and username=="username": #Meme command just for the lulz, because I'm a weeb with a daddy kink Kappa.
 				bot.sendMessage(chat_id=update.message.chat_id, text="Y-You... <3 *Blushes*")
 				LAST_UPDATE_ID = update_id
